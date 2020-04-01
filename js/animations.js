5790690bde67faad212c1379e84de0fd724e3e60
Tissa Khosla <tissakhosla@Tissas-MacBook-Pro.local>
2020-04-01 15:37:39 -0400

    added the rest of the circle graphs 	- now we'll animate and add the grade scale!

--- js/animations.js
+++ js/animations.js
@@ -1,5 +1,7 @@
 var progress_95 = document.getElementsByClassName('progress')[0]
 var progress_80 = document.getElementsByClassName('progress')[1]
+var progress_100 = document.getElementsByClassName('progress')[2]
+var progress_90 = document.getElementsByClassName('progress')[3]
 
 document.addEventListener('scroll', function() {
     if (progress_95.getBoundingClientRect().top > -10 && progress_95.getBoundingClientRect().top < 550) {
@@ -7,5 +9,11 @@ document.addEventListener('scroll', function() {
     }
     if (progress_80.getBoundingClientRect().top > -10 && progress_80.getBoundingClientRect().top < 550) {
         progress_80.classList.add("animate-80")
+    }
+    if (progress_100.getBoundingClientRect().top > -10 && progress_100.getBoundingClientRect().top < 550) {
+        progress_100.classList.add("animate-100")
+    }
+    if (progress_90.getBoundingClientRect().top > -10 && progress_90.getBoundingClientRect().top < 550) {
+        progress_90.classList.add("animate-90")
     }
 })
