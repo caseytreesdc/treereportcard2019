0442bf241edf5de03fde4a4dfeccf22380ee1548
Tissa Khosla <tissakhosla@Tissas-MacBook-Pro.local>
2020-04-01 15:01:29 -0400

    entered one more animation and mechanism for different percentages.  - could be abstracted but we'll stay on this for now please

--- js/animations.js
+++ js/animations.js
@@ -1,7 +1,11 @@
-var progress = document.getElementsByClassName('progress')[0]
+var progress_95 = document.getElementsByClassName('progress')[0]
+var progress_80 = document.getElementsByClassName('progress')[1]
 
 document.addEventListener('scroll', function() {
-    if (progress.getBoundingClientRect().top > -10 && progress.getBoundingClientRect().top < 550) {
-        progress.classList.add("animate")
+    if (progress_95.getBoundingClientRect().top > -10 && progress_95.getBoundingClientRect().top < 550) {
+        progress_95.classList.add("animate-95")
+    }
+    if (progress_80.getBoundingClientRect().top > -10 && progress_80.getBoundingClientRect().top < 550) {
+        progress_80.classList.add("animate-80")
     }
 })
