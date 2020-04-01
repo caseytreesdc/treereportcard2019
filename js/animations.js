a144956553348cc8638b0a869d4c8379650b9e0e
Tissa Khosla <tissakhosla@Tissas-MacBook-Pro.local>
2020-04-01 13:05:29 -0400

    function to animate item only when in view is in place NEXT STEPS: 	- replace other starbursts 	- put in icons at the end 	- buils grading scale svg 	- animate it!

--- js/animations.js
+++ js/animations.js
@@ -1,2 +1,7 @@
-var radial = document.queryselector('radial')
-console.log(radial.getBoundingClientRect())
+var progress = document.getElementsByClassName('progress')[0]
+
+document.addEventListener('scroll', function() {
+    if (progress.getBoundingClientRect().top > -10 && progress.getBoundingClientRect().top < 550) {
+        progress.classList.add("animate")
+    }
+})
